634614fd088b72e2c26bc72dbf0734f4e966bd1d
Johnny Soto <Johnny_soto@outlook.com>
2020-03-31 05:43:26 -0500

    manage

--- eBookProgram-master/eBookManager.Engine/DeweyDecimal.cs
+++ eBookProgram-master/eBookManager.Engine/DeweyDecimal.cs
@@ -2,6 +2,7 @@
 {
     public class DeweyDecimal
     {
+        // model
         public string ComputerScience { get; set; } = "000";
         public string DataProcessing { get; set; } = "004";
         public string ComputerProgramming { get; set; } = "005";
